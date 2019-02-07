8968465314dc2df09679377a0406a833b15cea36
Tjitse Efdé <tjitse@vendic.nl>
2019-02-07 13:55:40 +0100

    Remove afterEach function

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import {VueStorefrontModule, VueStorefrontModuleConfig} from '@vue-storefront/core/lib/module'
 import {beforeEach} from './router/beforeEach'
-import {afterEach} from "./router/afterEach"
 import {routes} from './router/routes';
 
 const store = {
@@ -15,7 +14,7 @@ export const KEY = 'external-checkout'
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
   store: {modules: [{key: KEY, module: store}]},
-  router: {beforeEach, afterEach, routes}
+  router: {beforeEach, routes}
 }
 
 export const ExternalCheckout = new VueStorefrontModule(moduleConfig)
